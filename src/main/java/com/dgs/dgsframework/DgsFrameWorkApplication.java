c9ca02d03b13357b34c3ed53717ee6ec79384624
IZEX_WEB <http://192.168.100.91:3000/WEB/izexWTM>
2022-10-26 17:21:59 +0900

    # Author : 소현태 # Date          : 2022-10-26 # DB            : # Description   : [basic] 1. netflix dgsFrameWork Test

---
+++ src/main/java/com/dgs/dgsframework/DgsFrameWorkApplication.java
@@ -0,0 +1,13 @@
+package com.dgs.dgsframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DgsFrameWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DgsFrameWorkApplication.class, args);
+    }
+
+}
